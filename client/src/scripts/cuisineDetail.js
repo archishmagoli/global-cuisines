214f1bae94b4fc36485fcf6d2e201c82524f1970
Archie Goli <agoli6@gatech.edu>
2026-03-01 16:09:11 -0500

    debug routing

--- client/src/scripts/cuisineDetail.js
+++ client/src/scripts/cuisineDetail.js
@@ -5,14 +5,15 @@ const id = params.get('id')
 
 const getCuisine = async () => {
     if (!id || isNaN(id)) {
-        window.location.href = '/404.html'
+        window.location.href = '/404'
         return
     }
 
+    console.log(`${import.meta.env.VITE_API_URL}/cuisines/${id}`);
     const response = await fetch(`${import.meta.env.VITE_API_URL}/cuisines/${id}`);
 
     if (!response.ok) {
-        window.location.href = '/404.html'
+        window.location.href = '/404'
         return
     }
     const data = await response.json();
